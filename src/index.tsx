b3d7ce0c0eb4406782c5af77172a9ee2b05f1512
michael <abdulazizklichev1@gamil.com>
2024-03-22 11:41:31 +0900

    fix:MUI customized integration

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import './css/index.css';
-import theme from './theme';
+import theme from './app/MaterialTheme';
 
 
 
