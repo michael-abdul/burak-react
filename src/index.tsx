f17f7ae72e743de35cf12d29b79a1abd2e831c74
michael <abdulazizklichev1@gamil.com>
2024-03-22 11:24:26 +0900

    feat: MUI standard integration

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,21 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import './css/index.css';
+import theme from './theme';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
+      </ThemeProvider>
+
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
