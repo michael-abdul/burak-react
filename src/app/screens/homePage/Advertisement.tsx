906a23a28456e5207decbfcfca337857c5554e75
michael <abdulazizklichev1@gamil.com>
2024-03-29 15:29:29 +0900

    feat: develop advertisement sectional component

--- src/app/screens/homePage/Advertisement.tsx
+++ src/app/screens/homePage/Advertisement.tsx
@@ -2,7 +2,19 @@ import React from "react";
 
 export default function Advertisement(){
     return (
-       <div>Advertisement</div>
+       <div className="ads-restaurant-frame">
+        <video 
+         className={"ads-video"}
+         autoPlay={true}
+         loop
+         muted
+         playsInline
+         data-video-media=""
+         >
+            <source type="video/mp4" src="video/burak-ads.mp4"/>
+         </video>
+      
+       </div>
           
-    )
-    }
+    );
+}
