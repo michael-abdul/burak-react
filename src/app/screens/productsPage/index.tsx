b65733f7b9156afbfb4e077d664733f64a5fe3b3
michael <abdulazizklichev1@gamil.com>
2024-03-31 13:04:55 +0900

    feat:develop productPage nested routing system

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,5 +1,20 @@
+import React from 'react';
 import { Container } from "@mui/material";
-
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import ChosenProduct from './ChosenProduct';
+import Products from './Products';
 export  default function ProductsPage() {
-    return <Container>ProductsPage</Container>;
+const products = useRouteMatch();
+console.log(products);
+
+    return <div className={"products-page"}>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>;
   }
