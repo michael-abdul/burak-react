722458459d4bf2f8a6d0f2598974ea4e677f54aa
michael <abdulazizklichev1@gamil.com>
2024-04-14 17:13:51 +0900

    feat: Test class component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,48 +1,46 @@
-import React from 'react';
+import React from "react";
 
-import { Box,Button,Container, Stack, Typography } from '@mui/material';
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 
-import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import HomePage from './screens/homePage';
-import ProductsPage  from './screens/productsPage';
-import OrdersPage  from './screens/ordersPage';
-import  UserPage  from './screens/userPage';
-import HelpPage  from './screens/helpPage';
-import  OtherNavbar  from './components/headers/OtherNavbar';
-import HomeNavbar  from './components/headers/HomeNavbar';
-import Footer  from './components/footer';
-
-import '../css/app.css';
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import HelpPage from "./screens/helpPage";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import Footer from "./components/footer";
+import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import Test from "./screens/test";
 
 function App() {
   const location = useLocation();
   return (
-  <>
- 
-  {location.pathname === '/' ? <HomeNavbar/> : <OtherNavbar/>}
-  <Switch>
-    <Route path="/products">
-      <ProductsPage />
-    </Route>
-    <Route path="/orders">
-      <OrdersPage />
-    </Route>
-    <Route path="/member-page">
-      <UserPage />
-    </Route>
-    <Route path="/help">
-      <HelpPage />
-    </Route>
-    <Route path="/">
-      <HomePage />
-    </Route>
-  </Switch>
-  <Footer/>
-</>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/">
+          <Test />
+          {/* <HomePage /> */}
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
-
-
 }
 export default App;
