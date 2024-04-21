24a444475af64db5ec80d46f1bddde8d82a677a2
michael <abdulazizklichev1@gamil.com>
2024-04-21 15:15:19 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -28,50 +28,7 @@ export default function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
   const { popularDishes } = useSelector(popularDishesRetriever);
   //Selector: Store=> Data
-  useEffect(() => {
-    //Backend server data request=> DATA
-    const result = [
-      {
-        _id: "6624a316ec25574a54fbc394",
-        productStatus: "PROCESS",
-        productCollection: "DISH",
-        productName: "Steak",
-        productPrice: 15,
-        productLeftCount: 1,
-        productSize: "NORMAL",
-        productVolume: 1,
-        productDesc: "This is Steak",
-        productImages: [
-          "uploads/products/24faf2b5-e9e9-4573-b1e5-66ac77db6a81.jpg",
-        ],
-        productViews: 0,
-        createdAt: "2024-04-21T05:24:38.131Z",
-        updatedAt: "2024-04-21T05:50:31.022Z",
-        __v: 0,
-      },
-      {
-        _id: "6624a340ec25574a54fbc397",
-        productStatus: "PROCESS",
-        productCollection: "DISH",
-        productName: "Kebab",
-        productPrice: 12,
-        productLeftCount: 10,
-        productSize: "LARGE",
-        productVolume: 1,
-        productDesc: "Kebab",
-        productImages: [
-          "uploads/products/81f02bc3-e3c9-4be0-8841-452bc4d63b6e.jpg",
-        ],
-        productViews: 0,
-        createdAt: "2024-04-21T05:25:20.899Z",
-        updatedAt: "2024-04-21T05:50:32.608Z",
-        __v: 0,
-      },
-    ];
-    //Slice: Data => Store
-    // @ts-ignore
-    setPopularDishes(result);
-  }, []);
+  useEffect(() => {}, []);
   console.log("popularDishes", popularDishes);
   return (
     <div className={"homepage"}>
