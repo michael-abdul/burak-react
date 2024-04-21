e55a5c477958ddc6f463cb9e16e7cfaa37177237
michael <abdulazizklichev1@gamil.com>
2024-04-21 14:04:50 +0900

    feat: create homePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,20 +1,27 @@
-import React from "react";
-import { Container } from "@mui/material";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import NewDishes from "./NewDishes";
 import ActiveUsers from "./ActiveUsers";
 import PopularDishes from "./PopularDishes";
 import Advertisement from "./Advertisement";
 import Events from "./Events";
-import "../../../css/home.css"
+import "../../../css/home.css";
 
 export default function HomePage() {
-    return <div className={"homepage"}>
-   <Statistics/>
-   <PopularDishes/>
-   <NewDishes/>
-   <Advertisement/>
-   <ActiveUsers/>
-   <Events/>
+  //Selector: Store=> Data
+  useEffect(() => {
+    //Backend server data request=> DATA
+    //Slice: Data => Store
+  }, []);
+
+  return (
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
     </div>
-  }
+  );
+}
