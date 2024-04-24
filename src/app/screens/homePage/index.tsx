a8908bde6ab6bb3d75b9f3aca96f6d0ed9ce8bad
michael <abdulazizklichev1@gamil.com>
2024-04-24 20:05:46 +0900

    fix: retrieve fresh menu and modify NewDishes component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,22 +5,23 @@ import ActiveUsers from "./ActiveUsers";
 import PopularDishes from "./PopularDishes";
 import Advertisement from "./Advertisement";
 import Events from "./Events";
-import "../../../css/home.css";
 
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
   //Selector: Store=> Data
   useEffect(() => {
     const product = new ProductService();
@@ -34,6 +35,19 @@ export default function HomePage() {
       .then((data) => {
         console.log("data passed here", data);
         setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        console.log("data passed here", data);
+        setNewDishes(data);
       })
       .catch((err) => console.log(err));
   }, []);
