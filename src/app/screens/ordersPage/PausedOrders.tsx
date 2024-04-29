252a5fa4fa20e3db609e03d74fcb48b59f4717a1
michael <abdulazizklichev1@gamil.com>
2024-04-29 11:19:08 +0900

    fix: modify ProcessOrders and FinishedOrders component

--- src/app/screens/ordersPage/PausedOrders.tsx
+++ src/app/screens/ordersPage/PausedOrders.tsx
@@ -20,6 +20,8 @@ const pausedOrdersRetriever = createSelector(
 export default function PausedOrders() {
   const { pausedOrders } = useSelector(pausedOrdersRetriever);
 
+
+  /** Handlers**/
   return (
     <TabPanel value={"1"}>
       <Stack>
--- src/app/screens/ordersPage/ProcessOrders.tsx
+++ src/app/screens/ordersPage/ProcessOrders.tsx
@@ -3,71 +3,93 @@ import { Box, Stack } from "@mui/material";
 import Button from "@mui/material/Button";
 import TabPanel from "@mui/lab/TabPanel";
 import moment from "moment";
-export default function ProcessOrders () { 
-    return(
-    <TabPanel value={"2"}>
-        <Stack>
-            {[1, 2].map((ele, index) => {
-                return (
-                    <Box key={index} className={"order-main-box" }>
-                            <Box className={"order-box-scroll"}>
-                                {[1, 2].map((ele2, index2) => {
-                                    return (
-                                    <Box key={index2} className={"orders-name-price"}>
-                                    <img
-                                    src={"/img/kebab.webp"}
-                                    className={"order-dish-img"}
-                                    />
-                                    <p className={"title-dish"}>Kebab</p>
-                                    <Box className={"price-box"}>
-                                    <p>$11</p>
-                                    <img src={"/icons/close.svg"} />
-                                    <p>2</p>
-                                    <img src={"/icons/pause.svg"} />
-                                <p style={{ marginLeft: "15px" }}>$24</p>
-                            </Box>
-                        </Box>
-                    ) ;
-                    })}
-                    </Box>
 
-                <Box className={"total-price-box"}>
-                    <Box className={"box-total"}>
-                        <p>Product price</p>
-                        <p>$22</p>
-                        <img src={"/icons/plus.svg"} style={{ marginLeft: "20px"}}/>
-                        <p>Delivery cost</p>
-                        <p>$2</p>
-                        <img
-                        src={"/icons/pause.svg"}
-                        style={{ marginLeft: "20px" }}
-                        />
-                        <p>Total</p>
-                        <p>$24</p>
-                        </Box>
-                        <p className={"data-compl"}>
-                            {moment().format("YYYY-MM-DD HH:mm")}
-                        </p>
-                        <Button 
-                        variant="contained" 
-                     
-                        className={"verify-button"}
-                        >
-                            Verify to Fulfil
-                        </Button>
-                 
-                            </Box>
-                            </Box>
-                            );
-            })}
-            {false && (
-                <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
-                    <img src={"/icons/noimage-list.svg"}
-                    style={{ width:300, height:300}}
-                    />
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveProcessOrders } from "./selector";
+
+import { serverApi } from "../../../lib/config";
+import { Order, OrderItem } from "../../../lib/types/order";
+import { Product } from "../../../lib/types/product";
+/** REDUX SLICE & SELECTOR **/
+
+const processOrdersRetriever = createSelector(
+  retrieveProcessOrders,
+  (processOrders) => ({ processOrders })
+);
+export default function ProcessOrders() {
+  const { processOrders } = useSelector(processOrdersRetriever);
+
+  return (
+    <TabPanel value={"2"}>
+      <Stack>
+        {processOrders?.map((order: Order) => {
+          return (
+            <Box key={order._id} className={"order-main-box"}>
+              <Box className={"order-box-scroll"}>
+                {order?.orderItems.map((item: OrderItem) => {
+                  const product: Product = order.productData.filter(
+                    (ele: Product) => item.productId === ele._id
+                  )[0];
+                  const imagePath = `${serverApi}/${product.productImages[0]}`;
+                  return (
+                    <Box key={item._id} className={"orders-name-price"}>
+                      <img src={imagePath} className={"order-dish-img"} />
+
+                      <p className={"title-dish"}>{product.productName}</p>
+
+                      <Box className={"price-box"}>
+                        <p>${item.itemPrice}</p>
+                        <img src={"/icons/close.svg"} />
+                        <p>{item.itemQuantity}</p>
+                        <img src={"/icons/pause.svg"} />$
+                        {item.itemQuantity * item.itemPrice}
+                      </Box>
+                    </Box>
+                  );
+                })}
+              </Box>
+
+              <Box className={"total-price-box"}>
+                <Box className={"box-total"}>
+                  <p>Product price</p>
+                  <p>${order.orderTotal - order.orderDelivery}</p>
+
+                  <img src={"/icons/plus.svg"} style={{ marginLeft: "20px" }} />
+                  <p>Delivery cost</p>
+                  <p>${order.orderDelivery}</p>
+
+                  <img
+                    src={"/icons/pause.svg"}
+                    style={{ marginLeft: "20px" }}
+                  />
+                  <p>Total</p>
+                  <p>${order.orderTotal}</p>
                 </Box>
-            )}
-            </Stack>
-            </TabPanel>
-        );
-    }
+                <p className={"data-compl"}>
+                  {moment().format("YYYY-MM-DD HH:mm")}
+                </p>
+                <Button variant="contained" className={"verify-button"}>
+                  Verify to Fulfil
+                </Button>
+              </Box>
+            </Box>
+          );
+        })}
+        {!processOrders ||
+          (processOrders.length === 0 && (
+            <Box
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"center"}
+            >
+              <img
+                src={"/icons/noimage-list.svg"}
+                style={{ width: 300, height: 300 }}
+              />
+            </Box>
+          ))}
+      </Stack>
+    </TabPanel>
+  );
+}
